23723038c6ea1ec9b7ade8826b28f404b1cc9fb9
jijiadvik <jijiadvik.com>
2023-03-01 15:28:33 +0400

    addeded

---
+++ firstinmakespjt/firstinmakesapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FirstinmakesappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'firstinmakesapp'
---
+++ firstinmakespjt/firstinmakespjt/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for firstinmakespjt project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'firstinmakespjt.settings')
+
+application = get_asgi_application()
